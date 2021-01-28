8da594e14bafb37a0b103ad5ea9e921a088b675e
yseries <57162864+yseries@users.noreply.github.com>
2021-01-29 02:03:18 +0800

    create new

---
+++ doubleball/src/main/java/com/yseries/doubleball/Doubleball.java
@@ -0,0 +1,13 @@
+package com.yseries.doubleball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleballApplication.class, args);
+	}
+
+}
---
+++ doubleball/src/test/java/com/yseries/doubleball/DoubleballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yseries.doubleball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubleballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
